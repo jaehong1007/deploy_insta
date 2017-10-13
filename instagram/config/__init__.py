91e4c8f14ea32243db4aef70e39776ef04b7a861
Jaehong Kim <jaehong1007@gmail.com>
2017-10-13 16:22:06 +0900

    Post 앱 생성, Post,PostComment 모델 작성

---
+++ instagram/config/__init__.py
@@ -0,0 +1,11 @@
+"""
+post앱을 생성
+class Post(models.Model):
+   photo = models.어떤필드를 써야할까요
+   생성일시 기록
+
+class PostComment:
+    photo = 자신의 Photo와 MTO으로 연결
+    생성일시 기록
+"""
+
--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_extensions',
+    'post',
 ]
 
 MIDDLEWARE = [
