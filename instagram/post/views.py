6210350fe7d3f3d77d5d7c29f34dabf305916dd9
Jaehong Kim <jaehong1007@gmail.com>
2017-10-13 16:56:40 +0900

    post_list view 작성

--- instagram/post/views.py
+++ instagram/post/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from post.models import Post
+
+
+def post_list(request):
+    """
+      모든 Post 목록을 리턴,
+      template은 'post/post_list.html'을 사용,
+    """
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'post/post_list.html', context)
