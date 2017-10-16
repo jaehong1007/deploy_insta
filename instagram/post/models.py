c22a887a08bd3e09578aa065c645bd8940a26337
Jaehong Kim <jaehong1007@gmail.com>
2017-10-16 15:31:45 +0900

    댓글 보여주기 추가

--- instagram/post/models.py
+++ instagram/post/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
 
 
 class PostComment(models.Model):
-    post = models.ForeignKey(Post)
+    post = models.ForeignKey(Post, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
