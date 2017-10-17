dbd8c14126947c0adb4e7a3ebc86af32639c6705
Jaehong Kim <jaehong1007@gmail.com>
2017-10-17 15:05:15 +0900

    login form 작성

--- instagram/member/forms.py
+++ instagram/member/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import authenticate, login as django_login
 from django.contrib.auth.models import User
 
 
@@ -28,5 +29,37 @@ class SignupForm(forms.Form):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = None
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        user = authenticate(
+            username=username,
+            password=password
+        )
+        if not user:
+            raise forms.ValidationError('Invalid login credentials')
+
+    def login(self, request):
+        print(self.user)
--- instagram/post/forms.py
+++ instagram/post/forms.py
@@ -39,3 +39,4 @@ class CommentForm(forms.Form):
             }
         ),
     )
+
